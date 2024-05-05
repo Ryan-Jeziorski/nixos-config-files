27b6206c79fe87cdfdf710a2a85ebd3537482a42
Ryan-Jeziorski <59761454+Ryan-Jeziorski@users.noreply.github.com>
2024-05-04 22:16:42 -0500

    Update configuration.nix
    Update ssh agent auto start

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,8 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  programs.ssh.startAgent = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
