d45ec35bd47a01dd5b76aa658d881f175efa9a61
Ryan <rjeziorski@protonmail.com>
2024-06-12 22:10:32 -0500

    Revert "Update lock file"
    This reverts commit 7dd4b109aee62e4bfaa35efb46c0129ae7e36baa.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,6 @@
 
    inputs = {
      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-     #nix-vscode-extensions.follows = "nixpkgs";
-     #nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
    };
 
   outputs = { self, nixpkgs, ... }: {
@@ -16,23 +13,7 @@
           ./configuration.nix
           ./hardware-configurations/lumbridge/hardware-configuration.nix
           { networking.hostName = "lumbridge"; }
-          ]; 
-          #{
-          users.users.ryan = {
-            packages = with nixpkgs; [
-              logseq
-            ];
-#            (vscode-with-extensions.override {
-#              vscode = vscodium;
-#              vscodeExtensions = with vscode-extensions.extensions.x86_64-linux; [
-#                vscode-marketplace.bbenoist.nix
-#                vscode-marketplace.rust-lang.rust-analyzer
-#                vscode-marketplace.vscodevim.vim
-#                open-vsx.jeanp413.open-remote-ssh
-            #})
-	  #];
-          };#}
-        #];
+        ];
       };
       #myComputer = nixpkgs.lib.nixosSystem {
         #system = "x86_64-linux";
