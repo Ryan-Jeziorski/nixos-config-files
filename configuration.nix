7e84fb997271890199499f68fdd6080632f3826e
Ryan <rjeziorski@protonmail.com>
2024-06-13 22:00:11 -0500

    remove dead code

--- configuration.nix
+++ configuration.nix
@@ -8,14 +8,6 @@
   # Turn experimental features because flakes are too good.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-
-  # networking.hostName = "lumbridge"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -69,9 +61,6 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   #};
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ryan = {
