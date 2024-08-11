ae451ae21e7d14daac6e21ea4be2c8a10b8756ab
ryan@roshar <rjeziorski@protonmail.com>
2024-08-11 14:33:23 -0500

    Update VSCode to include vspacecode and which-key

--- programs/vscodium.nix
+++ programs/vscodium.nix
@@ -17,14 +17,14 @@ with config;
       (vscode-with-extensions.override {
         vscode = vscodium;
         vscodeExtensions = with vscode-extensions.extensions.${system}; [
-          extensions.vscode-marketplace.golang.go
-
           extensions.vscode-marketplace.bbenoist.nix
           extensions.vscode-marketplace.rust-lang.rust-analyzer
-          extensions.vscode-marketplace.vscodevim.vim
           extensions.vscode-marketplace.karunamurti.tera
           extensions.vscode-marketplace.samuelcolvin.jinjahtml
           extensions.open-vsx.jeanp413.open-remote-ssh
+          extensions.vscode-marketplace.vscodevim.vim
+          extensions.vscode-marketplace.vspacecode.whichkey
+          extensions.vscode-marketplace.vspacecode.vspacecode
         ];
       })
     ];
