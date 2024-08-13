a20b1d5e8e7555b15921f086859f34b6284e38cd
Ryan <rjeziorski@outlook.com>
2024-08-13 16:10:36 -0500

    Add nerdfonts

--- hosts/latitude-nix/configuration.nix
+++ hosts/latitude-nix/configuration.nix
@@ -102,6 +102,12 @@
   ];
 
 
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
