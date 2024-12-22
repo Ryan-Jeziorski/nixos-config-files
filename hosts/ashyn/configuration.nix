c13a42d312a3497cbaf035cb6b37d4ec4407e629
Ryan <rjeziorski@protonmail.com>
2024-12-21 18:39:17 -0600

    Swap to only opening the needed port, instead of all ports.

--- hosts/ashyn/configuration.nix
+++ hosts/ashyn/configuration.nix
@@ -38,10 +38,10 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
