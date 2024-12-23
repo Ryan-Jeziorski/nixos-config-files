a18a57be8a2d40fd5d77c138534bead5ebee670c
Ryan <rjeziorski@protonmail.com>
2024-12-22 20:51:57 -0600

    Enable firewall again.

--- hosts/ashyn/configuration.nix
+++ hosts/ashyn/configuration.nix
@@ -41,7 +41,7 @@
   networking.firewall.allowedTCPPorts = [ 8000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
